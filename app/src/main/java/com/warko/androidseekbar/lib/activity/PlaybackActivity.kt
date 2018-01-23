5ad0aaa8a1b2a51a25a6b31a033a97efe92adc4b
warko <dima.sholom@yalantis.com>
2018-01-24 00:28:55 +0200

    Implemented choosing video

--- app/src/main/java/com/warko/androidseekbar/lib/activity/PlaybackActivity.kt
+++ app/src/main/java/com/warko/androidseekbar/lib/activity/PlaybackActivity.kt
@@ -1,5 +1,7 @@
 package com.warko.androidseekbar.lib.activity
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.warko.androidseekbar.R
@@ -13,4 +15,14 @@ class PlaybackActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_playback)
     }
+
+    companion object {
+        private const val EXTRA_PATH = "path"
+
+        fun newIntent(context: Context, path: String): Intent =
+                Intent(context, PlaybackActivity::class.java).apply {
+                    putExtra(EXTRA_PATH, path)
+                }
+    }
+
 }
