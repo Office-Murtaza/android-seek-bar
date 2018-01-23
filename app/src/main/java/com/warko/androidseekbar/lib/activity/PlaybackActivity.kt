0a6b29f6127df6257a9fdea95458f5aa1db1965e
warko <dima.sholom@yalantis.com>
2018-01-23 23:33:43 +0200

    Created PlaybackActivity

---
+++ app/src/main/java/com/warko/androidseekbar/lib/activity/PlaybackActivity.kt
@@ -0,0 +1,16 @@
+package com.warko.androidseekbar.lib.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.warko.androidseekbar.R
+
+/**
+ * Created by warko on 1/23/18.
+ */
+class PlaybackActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_playback)
+    }
+}
--- app/src/main/java/com/warko/androidseekbar/sample/MainActivity.kt
+++ app/src/main/java/com/warko/androidseekbar/sample/MainActivity.kt
@@ -1,12 +1,17 @@
-package com.warko.androidseekbar
+package com.warko.androidseekbar.sample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.warko.androidseekbar.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnChoose.setOnClickListener {
+
+        }
     }
 }
